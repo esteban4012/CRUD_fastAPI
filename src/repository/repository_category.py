a546b8f0230253d462f5d06f46dbdd832785d702
esteban <esteban4012223@gmail.com>
2023-07-13 18:01:14 -0500

    creamos el archivo repository_category.py en el cual mapeamos las dos clases y creamos la funcion fech_category para obtener las category de la base de datos

---
+++ src/repository/repository_category.py
@@ -0,0 +1,15 @@
+
+from db import session
+from models.category import Category, Category_entity
+
+
+def to_category_entity(category: Category):
+    return Category_entity(id=category.id, description=category.description)
+
+
+def fech_category():
+    entities = session.query(Category_entity)
+    models = []
+    for entity in entities:
+        models.append(entity)
+    return models
