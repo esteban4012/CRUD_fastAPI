45e793a47696600804f3006057186b5d43050c55
esteban <esteban4012223@gmail.com>
2023-07-13 17:25:33 -0500

    creamos la clase Category_entity con los mismos atributos que esta en la tabla de la base de datos

--- src/models/category.py
+++ src/models/category.py
@@ -1,6 +1,27 @@
 from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column
+from typing import Optional
+import db
+
 
 class Category(BaseModel):
-    id : int
+    id : Optional[int] = None
     description : str
+
+
+class Category_entity(db.Base):
+
+    __tablename__ = "category"
+
+    id = Column(Integer,primary_key=True)
+    description = Column(String(200), nullable=False)
     
+
+    def __init__(self, id, description ):
+
+        self.id = id
+        self.description = description
+
+    def __repr__(self):
+        return f"category({self.id}, {self.description})"
