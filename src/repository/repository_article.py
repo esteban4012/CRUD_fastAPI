b4d6a349c117f8f150d62c4a72478e5707e260fd
esteban <esteban4012223@gmail.com>
2023-07-13 18:17:36 -0500

    creamos el archivo repository_article.py en el cual mapeamos las dos clases y creamos la funcion  fech_articulo para obtener los articulos de la base de datos

---
+++ src/repository/repository_article.py
@@ -0,0 +1,17 @@
+from db import session
+from models.article import Article, Article_entity
+
+
+def to_articulo_entity(articulo: Article):
+    return Article_entity(id=articulo.id, 
+                            list_price=articulo.price, 
+                            description=articulo.description, 
+                            category_id=articulo.id_category)
+
+
+def fech_articulo():
+    entities = session.query(Article_entity)
+    models = []
+    for entity in entities:
+        models.append(entity)
+    return models
